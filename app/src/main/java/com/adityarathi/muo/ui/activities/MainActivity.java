c4a534d5344eb9d49cea76378a3e0d7add94181e
Aditya Rathi <adityarathi95@gmail.com>
2016-04-26 05:01:08 +0530

    - customized smart tab layout with fragment and tentative design

--- app/src/main/java/com/adityarathi/muo/ui/activities/MainActivity.java
+++ app/src/main/java/com/adityarathi/muo/ui/activities/MainActivity.java
@@ -1,30 +1,20 @@
 package com.adityarathi.muo.ui.activities;
 
-import android.app.ProgressDialog;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.MediaStore;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
 
 import com.adityarathi.muo.R;
-import com.adityarathi.muo.asyncTasks.AsyncBuildLibraryTask;
-import com.adityarathi.muo.dbHelper.DBAccessHelper;
-import com.adityarathi.muo.dbHelper.MediaStoreAccessHelper;
-import com.adityarathi.muo.ui.adapters.SongsAdapter;
-import com.adityarathi.muo.ui.fragments.BlankFragment;
+import com.adityarathi.muo.ui.customViews.SmartViewPager;
 import com.adityarathi.muo.ui.fragments.TracksListFragment;
-import com.adityarathi.muo.ui.objects.Song;
 import com.adityarathi.muo.utils.Common;
 import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.materialdrawer.Drawer;
@@ -32,10 +22,7 @@ import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -43,10 +30,14 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.viewpager) SmartViewPager viewPager;
+    @Bind(R.id.type_tabs) SmartTabLayout tabs;
+    @Bind(R.id.collapse_toolbar) CollapsingToolbarLayout collapsingToolbarLayout;
 
     public Context mContext;
     private Common mApp;
     private Drawer drawer;
+    private CustomTabAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mContext = this;
         mApp = (Common) mContext.getApplicationContext();
@@ -68,10 +60,9 @@ public class MainActivity extends AppCompatActivity {
                 .withActionBarDrawerToggleAnimated(true)
                 .withTranslucentStatusBar(true)
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withName("Tracks").withIcon(FontAwesome.Icon.faw_home),
-                        new PrimaryDrawerItem().withName("Playlists").withIcon(FontAwesome.Icon.faw_bell),
-                        new PrimaryDrawerItem().withName("Artists").withIcon(FontAwesome.Icon.faw_coffee),
-                        new PrimaryDrawerItem().withName("Albums").withIcon(FontAwesome.Icon.faw_group)
+                        new PrimaryDrawerItem().withName("Music").withIcon(FontAwesome.Icon.faw_home),
+                        new PrimaryDrawerItem().withName("Social Feed").withIcon(FontAwesome.Icon.faw_bell),
+                        new PrimaryDrawerItem().withName("Settings").withIcon(FontAwesome.Icon.faw_coffee)
 
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
@@ -80,18 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
                         if (drawerItem != null && drawerItem instanceof Nameable) {
                             String name = ((Nameable) drawerItem).getName().getText(MainActivity.this);
-                            getSupportActionBar().setTitle(name);
-                            Fragment fragment = null;
-                            switch (i) {
-                                case 0:
-                                    fragment = new TracksListFragment();
-                                    break;
-                                default:
-                                    fragment = new BlankFragment();
-                                    break;
 
-                            }
-                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                         }
 
                         return false;
@@ -116,7 +96,40 @@ public class MainActivity extends AppCompatActivity {
 
         drawer.setSelectionAtPosition(0, true);
 
+        adapter = new CustomTabAdapter(getSupportFragmentManager());
+        viewPager.setOffscreenPageLimit(15);
+        viewPager.setAdapter(adapter);
+        tabs.setCustomTabView(R.layout.custom_smart_tab_view1,R.id.tab_text);
+        tabs.setViewPager(viewPager);
+        viewPager.setCurrentItem(0);
 
+
+    }
+
+    public class CustomTabAdapter extends FragmentStatePagerAdapter {
+        private final String[] tab_names = {"RECENT", "SONGS", "ARTISTS", "ALBUMS","PLAYLISTS"};
+
+        private CustomTabAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tab_names[position];
+        }
+
+        @Override
+        public int getCount() {
+            return tab_names.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                default:
+                    return new TracksListFragment();
+            }
+        }
     }
 
 
